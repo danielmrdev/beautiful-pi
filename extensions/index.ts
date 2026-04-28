db583d7c34b383917d242896226427e1a582c886
Daniel Muñoz <github@danielmr.dev>
2026-04-28 18:47:20 +0200

    chore: wire settings extension in entry point

--- extensions/index.ts
+++ extensions/index.ts
@@ -1,8 +1,16 @@
 import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
-import bannerExtension from "./banner.ts";
-import footerExtension from "./footer.ts";
+import bannerExtension from "./banner/index.ts";
+import footerExtension from "./footer/index.ts";
+import toolsOneLineExtension from "./tools-one-line/index.ts";
+import assistantStyleExtension from "./assistant-style/index.ts";
+import userStyleExtension from "./user-style/index.ts";
+import settingsExtension from "./settings/index.ts";
 
 export default function (pi: ExtensionAPI) {
+  settingsExtension(pi);         // register /beautiful-pi first
   bannerExtension(pi);
   footerExtension(pi);
+  toolsOneLineExtension(pi);
+  assistantStyleExtension(pi);
+  userStyleExtension(pi);
 }
